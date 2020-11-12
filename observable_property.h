c80c3ce819f4dcf889a329cedc5f165e4a43ed6a
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2020-11-12 23:29:31 +0400

    Observable property gives the previous and the new values when changed

--- observable_property.h
+++ observable_property.h
@@ -11,26 +11,35 @@ namespace opensalad
 	namespace utils
 	{
 		template <typename T>
-		class observable_property : public i_observable<std::function<void()>>
+		class observable_property : public i_observable<std::function<void(T const&, T const&)>>
 		{
 		public:
 			observable_property()
 				: m_value()
 			{}
 
-			observable_property(T && initial_value)
+			observable_property(T const& initial_value)
+				: m_value()
+			{}
+
+			observable_property(T&& initial_value)
 				: m_value(initial_value)
 			{}
 
-			void changed()
+			void changed(T const& oldValue, T const& newValue)
 			{
 				for (auto observer : m_observers)
 				{
-					observer();
+					observer(oldValue, newValue);
 				}
 			}
 
 			observable_property<T>& operator = (T const& value)
+			{
+				return set_value(value);
+			}
+
+			observable_property<T>& operator = (T&& value)
 			{
 				return set_value(value);
 			}
@@ -40,7 +49,7 @@ namespace opensalad
 				return m_value;
 			}
 
-			operator const T&() const
+			operator const T& () const
 			{
 				return m_value;
 			}
@@ -52,19 +61,28 @@ namespace opensalad
 
 			observable_property<T>& set_value(T const& value)
 			{
+				T oldValue = m_value;
 				m_value = value;
-				changed();
+				changed(oldValue, m_value);
 				return *this;
 			}
-			
-			void observe(std::function<void()> const& observer) override
+
+			observable_property<T>& set_value(T&& value)
+			{
+				T oldValue = m_value;
+				m_value = value;
+				changed(oldValue, m_value);
+				return *this;
+			}
+
+			void observe(std::function<void(T const&, T const&)> const& observer) override
 			{
 				m_observers.push_back(observer);
 			}
 
 		private:
 			T m_value;
-			std::vector<std::function<void()>> m_observers;
+			std::vector<std::function<void(T const&, T const&)>> m_observers;
 		};
 	}
 }
